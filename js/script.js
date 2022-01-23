513ad8dd7b00724a3bb0682beab09aa8aa90f566
Azaliya <azaliya-frbi@rambler.ru>
2022-01-23 20:08:18 +0300

    меню - старт

---
+++ js/script.js
@@ -0,0 +1,222 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+  //  tabs
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader__items");
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      //   item.style.display = "none";
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
+
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabContent(i = 1) {
+    // tabsContent[i].style.display = "block";
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (item == target) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+  hideTabContent();
+  showTabContent();
+  //  timer
+  const deadline = "2022-12-31";
+  function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(total / (24 * 60 * 60 * 1000)),
+      hours = Math.floor((total / (60 * 60 * 1000)) % 24),
+      minutes = Math.floor((total / (60 * 1000)) % 60),
+      seconds = Math.floor((total / 1000) % 60);
+    // return {
+    //   'total': total,
+    //   'days': days,
+    //   'hours': hours,
+    //   'minutes': minutes,
+    //   'seconds': seconds
+    // }
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateclock, 1000);
+
+    updateclock();
+
+    function updateclock() {
+      const t = getTimeRemaining(endtime);
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock(".timer", deadline);
+  //  модальное окно
+  const modalTrigger = document.querySelectorAll("[data-modal]"),
+    // const modalTrigger = document.querySelector("[data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]");
+
+  function openModal() {
+    // modal.classList.add("show");
+    // modal.classList.remove("hide");
+    modal.classList.toggle("show");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  }
+
+  modalTrigger.forEach((item) => {
+    item.addEventListener("click", openModal);
+  });
+
+  function closeModal() {
+    // modal.classList.add("hide");
+    // modal.classList.remove("show");
+    modal.classList.toggle("show");
+    document.body.style.overflow = "";
+  }
+
+  modalCloseBtn.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+
+  //  открытие модалки по таймеру
+  // const modalTimerId = setTimeout(openModal, 5000);
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
+  window.addEventListener("scroll", showModalByScroll);
+  // { once: true }
+
+  //  Урок48: Используем классы для карточек
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 0.5;
+      // this.priceRu=price*this.transfer;
+      this.changeToRUB();
+    }
+
+    changeToRUB() {
+      this.priceRu = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = `
+        <div class="menu__item">
+          <img src=${this.src} alt=${this.alt}>
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">${this.descr}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${this.priceRu}</span> руб/день</div>
+          </div>
+        </div>
+      `;
+      this.parent.append(element);
+    }
+  }
+  const descrAzaMenu =
+    "Во многих фруктах можно найти практически всю таблицу Менделеева и все необходимые организму человека витамины. Во фруктах содержатся: бета-каротин, холин, витамины А, группы В (В1, В2, В5, В6, В9, В12), С, Е, К, Н и РР, а также калий, кальций, магний, цинк, селен, медь и марганец, железо, хлор и сера, йод, хром, фтор, молибден, бор и ванадий, олово и титан, кремний, кобальт, никель и алюминий, фосфор и натрий. По содержанию клетчатки фрукты не уступают злакам и овощам, а иногда и опережают их. Фрукты играют немаловажную роль в нормальном пищеварении и метаболизме, незаменимы в диетическом питании.";
+
+  const azaMenu = new MenuCard(
+    "img/tabs/fruits.jpg",
+    "фрукты",
+    'Меню "Фрукты"',
+    "Всегда свежие ягоды и фрукты: яблоки, киви, ананас, грейпфрут, виноград, клубника, вишня",
+    10,
+    ".menu .container"
+  );
+  azaMenu.render();
+
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    10,
+    ".menu .container"
+  ).render();
+
+  new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    "Меню “Премиум”",
+    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+    10,
+    ".menu .container"
+  ).render();
+
+  new MenuCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Постное"',
+    "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+    10,
+    ".menu .container"
+  ).render();
+});
